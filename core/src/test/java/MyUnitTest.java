68e345c55a879188351726ba35cbebe0614b6544
aymanish <80852613+aymanish@users.noreply.github.com>
2022-03-06 22:35:02 +0000

    Add files via upload

---
+++ core/src/test/java/MyUnitTest.java
@@ -0,0 +1,18 @@
+import com.mygdx.game.MyUnit;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+//Test to check if CI works:
+public class MyUnitTest {
+
+
+    @Test
+    public void testConcatenate() {
+        MyUnit myUnit = new MyUnit();
+
+        String result = myUnit.concatenate("one", "two");
+
+        assertEquals("onetw", result);
+
+    }
+}
